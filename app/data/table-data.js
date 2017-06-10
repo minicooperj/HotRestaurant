725bc1628583d0a61696f52691084998c8f82b8e
Cary Cooper <Cary Cooper>
2017-06-10 01:59:30 -0400

    Github Push

---
+++ app/data/table-data.js
@@ -0,0 +1,28 @@
+var tableArray = [
+  {
+    customerName: "Ahmed",
+    customerEmail: "ahmed@example.com",
+    customerID: "ahhaque89",
+    phoneNumber: "100-000-0000"
+  },
+  {
+    customerName: "JoJack",
+    customerEmail: "jjr@example.com",
+    customerID: "JoJack7",
+    phoneNumber: "200-000-0000"
+  },
+  {
+    customerName: "Matt Stone",
+    customerEmail: "MStone@example.com",
+    customerID: "MStone",
+    phoneNumber: "300-000-0000"
+  },
+  {
+    customerName: "Halle Kerry",
+    customerEmail: "halker@example.com",
+    customerID: "HallYeah",
+    phoneNumber: "400-000-0000"
+  }
+];
+
+module.exports = tableArray;
---
+++ app/data/waitinglist-data.js
@@ -0,0 +1,10 @@
+var waitingArray = [
+  {
+    customerName: "Saima",
+    customerEmail: "saima@example.com",
+    customerID: "saima89",
+    phoneNumber: "000-000-0000"
+  }
+];
+
+module.exports = waitingArray;
